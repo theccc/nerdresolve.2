5144fda1483f75f99996859bec7401bf8a81c9ce
Prelang Builder <builder@prelang.com>
2015-08-12 01:05:08 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Blog' to allow for create, update

--- app/admin/blog.rb
+++ app/admin/blog.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Blog do
+  permit_params :titulo, :subtitulo, :texto, :tags
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
