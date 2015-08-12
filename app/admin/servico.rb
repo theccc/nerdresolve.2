af1ddcc7c91dcb2d103755d446f25e0617b0bd63
Prelang Builder <builder@prelang.com>
2015-08-12 01:05:15 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Servico' to allow for create, update

--- app/admin/servico.rb
+++ app/admin/servico.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Servico do
+  permit_params :titulo, :descricao, :preco
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
