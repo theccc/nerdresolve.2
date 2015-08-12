c8dcce86fe84074c289e7d9ef78cf0e4fa471f1c
Prelang Builder <builder@prelang.com>
2015-08-12 01:05:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Nerd' to allow for create, update

--- app/admin/nerd.rb
+++ app/admin/nerd.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Nerd do
+  permit_params :nome, :sobrenome, :rg, :cpf, :nascimento, :endereco, :numero, :bairro, :cidade, :estado, :cep, :distancia, :especialidade1, :especialidade2, :especialidade3, :especialidade4, :especialidade5, :descricao, :taxa, :problema1, :problema2, :problema3, :problema4, :problema5, :problema6, :problema7, :problema8, :problema9, :problema10, :status
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
